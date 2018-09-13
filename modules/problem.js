b1ec40df842e53746b623bab138eda09aa30925d
komqaq <1047963985@qq.com>
2018-09-13 23:31:51 +0800

    qaz

--- modules/problem.js
+++ modules/problem.js
@@ -830,8 +830,8 @@ app.get('/problem/:id/testdata/download/:filename?', async (req, res) => {
       let problem = await Problem.fromID(id);
 
       if (!problem) throw new ErrorMessage('无此题目。');
-	  // modified
-      if (!await problem.isAllowedEditBy(res.locals.user)) throw new ErrorMessage('您没有权限进行此操作。');
+    // modified
+      if (!await problem.isAllowedEditBy(res.locals.user) && (filename.indexOf(".jpg")==-1) && (filename.indexOf(".jpeg")==-1) && (filename.indexOf(".png")==-1)) throw new ErrorMessage('您没有权限进行此操作。');
 	    if (!req.params.filename) {
         if (!await syzoj.utils.isFile(problem.getTestdataArchivePath())) {
           await problem.makeTestdataZip();
